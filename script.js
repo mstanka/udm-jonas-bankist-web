b57f001daebe5b9ae9bc2ee96ef86bf610b7b251
mstanka <mice78.mm@gmail.com>
2021-02-24 21:34:23 +0100

    reveal sections

--- script.js
+++ script.js
@@ -183,6 +183,29 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 
 headerObserver.observe(header);
 
+///////////////////////////////////////
+//// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserver(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
 //**************Experimenting*************************///
 //////////////////////////////////////////////
 //////////////////////////////////////////////

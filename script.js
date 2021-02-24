3f6d4f5893e96af933b28591edffaaa21d9b1248
mstanka <mice78.mm@gmail.com>
2021-02-24 21:56:41 +0100

    lazy loading img

--- script.js
+++ script.js
@@ -189,7 +189,6 @@ const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
@@ -205,6 +204,33 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+///////////////////////////////////////
+//// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+ 
+  if (!entry.isIntersecting) return;
+
+  // replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px', // start loading before we reach them
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
 
 //**************Experimenting*************************///
 //////////////////////////////////////////////
